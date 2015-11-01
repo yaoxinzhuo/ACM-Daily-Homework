033b87b5f9e2b2fa860c2697e8174dd591e3d15d
YaoxinZhuo <zhuoyaoxin@gmail.com>
2015-11-02 06:37:26 +0800

    A problem about (A^B)%C

---
+++ HDOJ1420_MyHomework.cpp
@@ -0,0 +1,55 @@
+/*
+	*Problem Description
+	Calculate the result of (A^B)%C,(A,B,C<=1000000)
+	
+	*Input
+	First input N,means that has N tests,then N lines datas,each has A,B,C;
+	
+	*Output
+	The result of (A^B)%C,one result per line
+	
+	*Sample Input
+	3
+	2 3 4
+	3 3 5
+	4 4 6
+	
+	*Sample Output
+	0
+	2
+	4
+*/
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+	long long int N;
+	long long int a,b,c;
+	cin>>N;
+	while(N--)
+	{
+		cin>>a>>b>>c;
+		long long int r=1%c;
+		for(;;)
+		{
+			if(b==0)
+			{
+				break;
+			}
+			if((b%2)==0)
+			{
+				b/=2;
+				a=(a*a)%c;
+			}
+			else
+			{
+				r=(r*a)%c;
+				b-=1;
+			}
+		}
+		cout<<r<<endl;
+	} 
+	return 0;
+}
